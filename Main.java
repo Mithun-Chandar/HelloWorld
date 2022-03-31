dbed505e6130189bfcd6feebf4dcdd3ee6ed4e60
Mithun <mithunchandar16@gmail.com>
2022-03-31 22:18:54 +0200

    First Commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String FirstName= "Mithun";
+        String SecondName ="Nageshwaran Sivagami";
+        int ID= 53135;
+
+        System.out.println(("Hello world.\nMy name is " + FirstName +" " + SecondName + "\n" +"Student ID: "+ ID));
+    }
+}
